5d01b819a1a244eaa12c0e69b254ccbc3d12cd91
Nourhene Ben Othmen <benothmennourhen8@gmail.com>
2026-06-28 21:14:49 +0100

    Creer stats dynamiques depuis roadmap, commands, labs, deployment

---
+++ data/heroStats.ts
@@ -0,0 +1,31 @@
+import type { HeroStat } from '@/types';
+import { roadmapSteps } from './roadmap';
+import { commandCategories } from './commands';
+import { correctedLab, practiceLab } from './labs';
+import { deploymentGuides } from './deployment';
+
+const totalCommands = commandCategories.reduce(
+  (acc, cat) => acc + cat.commands.length,
+  0
+);
+
+const totalLabs = (correctedLab ? 1 : 0) + (practiceLab ? 1 : 0);
+
+export const heroStats: HeroStat[] = [
+  {
+    label: 'Roadmap Steps',
+    value: roadmapSteps.length,
+  },
+  {
+    label: 'Commands',
+    value: `${totalCommands}+`,
+  },
+  {
+    label: 'Labs',
+    value: totalLabs,
+  },
+  {
+    label: 'Deployment Guides',
+    value: deploymentGuides.length,
+  },
+];
