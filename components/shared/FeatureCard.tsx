f0ac21248d04ebb2dd606de184d1b50922be922c
Nourhene Ben Othmen <benothmennourhen8@gmail.com>
2026-06-28 22:37:58 +0100

    style: add explicit hover border to feature card

--- components/shared/FeatureCard.tsx
+++ components/shared/FeatureCard.tsx
@@ -8,7 +8,7 @@ interface FeatureCardProps {
 
 export default function FeatureCard({ icon, title, description }: FeatureCardProps) {
   return (
-    <div className="glass-card p-6 group cursor-default">
+    <div className="glass-card p-6 group cursor-default hover:border-docker-500/20">
       <div className="w-12 h-12 rounded-lg bg-docker-500/10 flex items-center justify-center text-docker-400 mb-4 group-hover:bg-docker-500/20 group-hover:scale-110 transition-all duration-300">
         {icon}
       </div>
