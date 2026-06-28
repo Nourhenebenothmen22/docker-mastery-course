19fe63b4837df0fff9141ea36fb550f32d28ae66
Nourhene Ben Othmen <benothmennourhen8@gmail.com>
2026-06-28 23:10:00 +0100

    refactor: use next/image for hero background docker logo

--- components/DockerHeroBackground.tsx
+++ components/DockerHeroBackground.tsx
@@ -2,15 +2,27 @@ import Image from 'next/image';
 
 export default function DockerHeroBackground() {
   return (
-    <div className="absolute inset-0 pointer-events-none overflow-hidden" aria-hidden="true">
-      <Image
-        src="/docker-logo.png"
-        alt=""
-        fill
-        priority
-        className="object-contain opacity-5 blur-2xl scale-150 md:scale-[2]"
-        sizes="100vw"
-      />
+    <div className="absolute inset-0 overflow-hidden" aria-hidden="true">
+      <div className="absolute -top-40 -right-40 w-80 h-80 opacity-[0.03]">
+        <Image
+          src="/docker-logo.png"
+          alt=""
+          width={320}
+          height={320}
+          className="object-contain"
+          aria-hidden="true"
+        />
+      </div>
+      <div className="absolute -bottom-40 -left-40 w-96 h-96 opacity-[0.02] rotate-180">
+        <Image
+          src="/docker-logo.png"
+          alt=""
+          width={384}
+          height={384}
+          className="object-contain"
+          aria-hidden="true"
+        />
+      </div>
     </div>
   );
 }
